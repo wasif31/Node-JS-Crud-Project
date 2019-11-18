df725bc941e60d394e9e5ba2a2e1a78f1004ee99
wasif31 <mdwasif31@student.sust.edu>
2019-11-19 02:34:13 +0600

    database connected

--- Authentication/index.js
+++ Authentication/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 //import the route
 const authRoute = require('./auth');
 //route middlewire
+dotenv.config();
+// Connecting to Database
+mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true },
+    () => console.log('connected to db')
+);
 app.use('/api/user', authRoute);
 app.listen(5000, () => console.log('server runing now'));
