90952d1465a62401396c1554b06c47f2a23e128f
wasif31 <mdwasif31@student.sust.edu>
2019-11-19 05:33:14 +0600

    login done

--- Authentication/auth.js
+++ Authentication/auth.js
@@ -30,10 +30,32 @@ router.post('/register', async (req, res) => {
     try {
         const savedUser = await user.save();
         console.log(savedUser)
-        res.send(savedUser);
+        res.send({ user: user._id });
     } catch (err) {
         res.status(400).send(err);
     }
 });
+
+// LOGIN
+router.post('/login', async (req, res) => {
+    // Validate data
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // check if is already in database
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Email does not match');
+    // Password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send('Invalid Password');
+
+    res.status(200).send("success");
+    // Create and assign a token
+    //const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    //res.header('auth-token', token).send(token);
+
+});
+
+
 
 module.exports = router;
