6f84d3102093d9cf0968e3b9aa20b0650755ac98
wasif31 <mdwasif31@student.sust.edu>
2019-11-19 01:49:43 +0600

    first post test localhost:5000api/user/register

--- Authentication/index.js
+++ Authentication/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
-
-
+//import the route
+const authRoute = require('./auth');
+//route middlewire
+app.use('/api/user', authRoute);
 app.listen(5000, () => console.log('server runing now'));
