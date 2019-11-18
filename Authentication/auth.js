48d9689ece417b69811899fcd3cd26b8d50b40b8
wasif31 <mdwasif31@student.sust.edu>
2019-11-19 04:37:49 +0600

    validation for error and message
    validation for error and email

--- Authentication/auth.js
+++ Authentication/auth.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 const User = require('./models/User');
+//validation
+const Joi = require('@hapi/joi');
+const schema = {
+    name: Joi.string().min(5).required(),
+    email: Joi.string().min(9).required().email(),
+    birthday: Joi.string().min(6).required(),
+    phone: Joi.string().min(11).required(),
+    password: Joi.string().min(5).required()
+};
 
 
 router.post('/register', async (req, res) => {
+    // Validate data before making user
+    const { error } = Joi.validate(req.body, schema);
+    if (error) return res.status(400).send(error.details[0].message);
     const user = new User({
         name: req.body.name,
         email: req.body.email,
