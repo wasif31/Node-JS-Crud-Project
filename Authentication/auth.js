88fa460d1fa628e7943efaa05e1eaddb65f12f9a
wasif31 <mdwasif31@student.sust.edu>
2019-11-19 05:16:16 +0600

    password encryption done

--- Authentication/auth.js
+++ Authentication/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 //validation
 
 const { registerValidation, loginValidation } = require('./validation');
@@ -10,12 +11,20 @@ router.post('/register', async (req, res) => {
     //const { error } = Joi.validate(req.body, schema);
     const { error } = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
+
+    // checking authenticity that email not taken
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send('Email already Exists');
+    //password encryption
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);//gensalt=complexity=10 level
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
         birthday: req.body.birthday,
         phone: req.body.phone,
-        password: req.body.password
+        password: hashedPassword
     });
     console.log(user)
     try {
