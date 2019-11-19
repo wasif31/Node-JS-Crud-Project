4c030ec8e4a16abf70ea9f2deb8b2903121054b2
wasif31 <mdwasif31@student.sust.edu>
2019-11-19 06:07:18 +0600

    jwt implemented

--- Authentication/auth.js
+++ Authentication/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 //validation
 
 const { registerValidation, loginValidation } = require('./validation');
@@ -49,10 +50,10 @@ router.post('/login', async (req, res) => {
     const validPass = await bcrypt.compare(req.body.password, user.password);
     if (!validPass) return res.status(400).send('Invalid Password');
 
-    res.status(200).send("success");
+    //res.status(200).send("success");
     // Create and assign a token
-    //const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
-    //res.header('auth-token', token).send(token);
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
 
 });
 
